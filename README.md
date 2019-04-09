69d3252cf764984d34507602bb10b089f94dca37
Maxence Bouret <maxence.bouret@gmail.com>
2019-04-09 17:58:44 +0100

    Update

--- README.md
+++ README.md
@@ -16,3 +16,24 @@ $ npm install express hbs --save
 # Create a file "app.js" (can be also done manually without the command line)
 $ touch app.js
 ```
+
+## How to launch the project?
+```sh
+$ nodemon app.js
+```
+
+## How to create a new page?
+
+Let's say we want a page http://localhost:3000/carrot that displays "I am the super 🥕"
+
+First, we have to create a new route "GET /carrot"
+```js
+app.get('/carrot', (req,res,next) => {
+  res.render('carrot') // render "/views/carrot.hbs"
+})
+```
+
+Second, we have to create a file "/views/carrot.hbs"
+```hbs
+<p>I am the super 🥕</p>
+```
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('public'));
+
 // Define the views directory. It's "/views"
 app.set('views', __dirname + '/views')
 // Define the view engine: HBS
@@ -9,14 +11,31 @@ app.set('view engine', 'hbs')
 
 // Route GET http://localhost:3000/
 app.get('/', (req,res,next) => {
+  let isItMorning = (new Date()).getHours() <= 12
+
   let data = {
+    isItMorning,
     name: "Maxence",
-    bootcamp: "Ironhack WebDev Lisbon"
+    bootcamp: "Ironhack WebDev <strong>Lisbon</strong>",
+    cities: ['Madrid','Barcelona','Paris','Amsterdam','Berlin','Lisbon'],
   };
 
   // Render the file "/views/index.hbs"
   res.render('index', data)
 })
+
+// Route GET http://localhost:3000/best-teacher
+app.get('/best-teacher', (req,res,next) => {
+  let teachers = ['Joaquim','Maxence','Mostafa']
+  let randomIndex = Math.floor(Math.random()*teachers.length)
+  let randomName = teachers[randomIndex]
+
+  // Render the file "/views/best-teacher.hbs"
+  res.render('best-teacher', {
+    name: randomName,
+  })
+})
+
 
 app.listen(3000, () => {
   console.log("App running on http://localhost:3000")
