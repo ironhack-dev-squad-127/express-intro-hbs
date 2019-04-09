7e788093f5d1234206e6e34b9413440d0aa4e286
Maxence Bouret <maxence.bouret@gmail.com>
2019-04-09 16:35:35 +0100

    Update

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express')
 
 const app = express()
 
+// Define the views directory. It's "/views"
+app.set('views', __dirname + '/views')
+// Define the view engine: HBS
+app.set('view engine', 'hbs')
+
+// Route GET http://localhost:3000/
 app.get('/', (req,res,next) => {
-  res.send('Hello World')
+  let data = {
+    name: "Maxence",
+    bootcamp: "Ironhack WebDev Lisbon"
+  };
+
+  // Render the file "/views/index.hbs"
+  res.render('index', data)
 })
 
 app.listen(3000, () => {
